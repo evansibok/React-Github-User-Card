f4e5e506d1d3ceab6d461789c268afd9e9728996
evansibok <evansibok@gmail.com>
2019-10-04 02:01:08 +0100

    completed project and reached MVP

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 
 import {
   Dropdown,
@@ -15,6 +14,38 @@ import {
   CardTitle,
   CardSubtitle
 } from "reactstrap";
+
+import styled from "styled-components";
+
+const StyledCon = styled.div`
+  margin: 0 auto;
+  height: auto;
+  max-width: 300px;
+  font-size: 1.3rem;
+
+  .cardTitle {
+    font-size: 1.7rem;
+    font-weight: 500;
+
+    .cardSub {
+      font-size: 1.3rem;
+      font-weight: 400;
+    }
+  }
+
+  .bio {
+    font-size: 1.5rem;
+    margin-top: 1.5em;
+    margin-bottom: 1.5em;
+  }
+`;
+
+const StyledFollow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1.5em;
+  margin-bottom: 1.5em;
+`;
 
 class App extends Component {
   constructor() {
@@ -47,40 +78,46 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <StyledCon>
         <h1>Github User Card!</h1>
-        <div>
-          <Card>
-            <CardBody>
-              <CardTitle>Name: {this.state.gitUser.name}</CardTitle>
-              <CardSubtitle>Username: {this.state.gitUser.login}</CardSubtitle>
-            </CardBody>
-            <img
-              width="100%"
-              src={this.state.gitUser.avatar_url}
-              alt="headshot"
-            />
-            <CardBody>
-              <CardText>Bio: {this.state.gitUser.bio}</CardText>
+        <Card>
+          <CardBody className="cardTitle">
+            <CardTitle>{this.state.gitUser.name}</CardTitle>
+            <CardSubtitle className="cardSub">
+              {this.state.gitUser.login}
+            </CardSubtitle>
+          </CardBody>
+          <img
+            width="100%"
+            src={this.state.gitUser.avatar_url}
+            alt="headshot"
+          />
+          <CardBody>
+            <CardText className="bio">{this.state.gitUser.bio}</CardText>
+            <StyledFollow>
               <CardText>Following: {this.state.gitUser.following}</CardText>
               <CardText>Followers: {this.state.gitUser.followers}</CardText>
+            </StyledFollow>
+
+            <StyledFollow>
               <CardLink href={this.state.gitUser.html_url}>
                 {this.state.gitUser.html_url}
               </CardLink>
-            </CardBody>
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-              <DropdownToggle caret>Followers List</DropdownToggle>
-              <DropdownMenu>
-                {this.state.gitFollowers.map(user => (
-                  <DropdownItem key={user.id}>
-                    <a href={user.html_url}>{user.login}</a>
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-          </Card>
-        </div>
-      </div>
+
+              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                <DropdownToggle caret>Followers List</DropdownToggle>
+                <DropdownMenu>
+                  {this.state.gitFollowers.map(user => (
+                    <DropdownItem key={user.id}>
+                      <a href={user.html_url}>{user.login}</a>
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </Dropdown>
+            </StyledFollow>
+          </CardBody>
+        </Card>
+      </StyledCon>
     );
   }
 }
