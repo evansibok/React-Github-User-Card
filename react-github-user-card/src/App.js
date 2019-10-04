e7edd2d7c4711f231855b9b2413185310f053002
evansibok <evansibok@gmail.com>
2019-10-04 01:30:32 +0100

    add reactstrap dependency and displayed user details in a card

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardLink,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
 
 class App extends Component {
-
   constructor() {
     super();
 
@@ -12,7 +24,7 @@ class App extends Component {
       gitUser: {},
       gitFollowers: [],
       dropdownOpen: false
-    }
+    };
 
     this.toggle = this.toggle.bind(this);
   }
@@ -24,46 +36,52 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.github.com/users/evansibok')
+    fetch("https://api.github.com/users/evansibok")
       .then(response => response.json())
-      .then(user => this.setState({ gitUser: user }))
+      .then(user => this.setState({ gitUser: user }));
 
-    fetch('https://api.github.com/users/evansibok/followers')
+    fetch("https://api.github.com/users/evansibok/followers")
       .then(response => response.json())
-      .then(user => this.setState({ gitFollowers: user }))
+      .then(user => this.setState({ gitFollowers: user }));
   }
 
   render() {
     return (
       <div className="App">
         <h1>Github User Card!</h1>
-        <div key={this.state.gitUser.id}>
-          <img src={this.state.gitUser.avatar_url} alt="headshot" />
-          <h3>Name: {this.state.gitUser.name}</h3>
-          <p>Username: {this.state.gitUser.login}</p>
-          <p>Bio: {this.state.gitUser.bio}</p>
-          <p>Following: {this.state.gitUser.following}</p>
-          <p>Followers: {this.state.gitUser.followers}</p>
-          <p>Profile: <a href={this.state.gitUser.html_url}>{this.state.gitUser.html_url}</a>
-          </p>
+        <div>
+          <Card>
+            <CardBody>
+              <CardTitle>Name: {this.state.gitUser.name}</CardTitle>
+              <CardSubtitle>Username: {this.state.gitUser.login}</CardSubtitle>
+            </CardBody>
+            <img
+              width="100%"
+              src={this.state.gitUser.avatar_url}
+              alt="headshot"
+            />
+            <CardBody>
+              <CardText>Bio: {this.state.gitUser.bio}</CardText>
+              <CardText>Following: {this.state.gitUser.following}</CardText>
+              <CardText>Followers: {this.state.gitUser.followers}</CardText>
+              <CardLink href={this.state.gitUser.html_url}>
+                {this.state.gitUser.html_url}
+              </CardLink>
+            </CardBody>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle caret>Followers List</DropdownToggle>
+              <DropdownMenu>
+                {this.state.gitFollowers.map(user => (
+                  <DropdownItem key={user.id}>
+                    <a href={user.html_url}>{user.login}</a>
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+          </Card>
         </div>
-
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle caret>
-            Followers List
-        </DropdownToggle>
-          <DropdownMenu>
-            {
-              this.state.gitFollowers.map(user => (
-                <DropdownItem key={user.id}><a href={user.html_url}>{user.login}</a></DropdownItem>
-              ))
-            }
-          </DropdownMenu>
-        </Dropdown>
-
-
       </div>
-    )
+    );
   }
 }
 
