2ee9299d44788a2e6f93bc09ec8233ba920ede1d
Evans Ibok <evansibok@gmail.com>
2019-12-03 14:58:46 +0100

    starting working with the data

--- react-github-user-card2/src/App.js
+++ react-github-user-card2/src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
 
+import data from './data'
+
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [
+        data,
+      ],
+    }
+  }
+
+
+
   render() {
+    console.log(data)
     return (
       <div>
+        <h1>Evans Ibok's Github Card</h1>
+        {
+          this.state.users.map(user => (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+              <h3>{user.login}</h3>
+              <div>
+                <img src={user.avatar_url} alt={`${user.name}'s headshot`} />
+              </div>
+              <p>Bio: {user.bio}</p>
+              <p>Followers: {user.followers}</p>
+              <p>Following: {user.following}</p>
+              <p>Profile Link: {user.url}</p>
+
+            </div>
+          ))
+        }
 
       </div>
     )
