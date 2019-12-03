dfcdf98c034e1532477a6a8fd8bd529354f7e439
Evans Ibok <evansibok@gmail.com>
2019-12-03 15:24:44 +0100

    displayed card details properly

--- react-github-user-card2/src/App.js
+++ react-github-user-card2/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import data from './data'
+import CardList from './components/card-list';
 
 export default class App extends Component {
 
@@ -14,6 +15,13 @@ export default class App extends Component {
     }
   }
 
+  componentDidMount() {
+
+    fetch(``)
+      .then()
+      .then()
+  }
+
 
 
   render() {
@@ -21,22 +29,7 @@ export default class App extends Component {
     return (
       <div>
         <h1>Evans Ibok's Github Card</h1>
-        {
-          this.state.users.map(user => (
-            <div key={user.id}>
-              <h1>{user.name}</h1>
-              <h3>{user.login}</h3>
-              <div>
-                <img src={user.avatar_url} alt={`${user.name}'s headshot`} />
-              </div>
-              <p>Bio: {user.bio}</p>
-              <p>Followers: {user.followers}</p>
-              <p>Following: {user.following}</p>
-              <p>Profile Link: {user.url}</p>
-
-            </div>
-          ))
-        }
+        <CardList users={this.state.users} />
 
       </div>
     )
